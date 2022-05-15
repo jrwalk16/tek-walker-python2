f337a7fbe431df3bdbb11f1e0450c4502a13a414
jrwalk16 <jeremywalker1216@Gmail.com>
2022-05-15 12:35:58 -0400

    added sqlalchemy packages to project

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
-
-
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://puppies.db'
 
 @app.route('/')
 
