469cc896ebaa95e0e60fa5091ef407b4ad61d9ea
jrwalk16 <jeremywalker1216@Gmail.com>
2022-05-15 23:45:36 -0400

    committed redo for assignment

--- hello.py
+++ hello.py
@@ -1,23 +1,57 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+
+
+# from tkinter.messagebox import RETRY
+from flask import Flask, redirect, url_for, session
+from flask import render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+
+
 
 app = Flask(__name__)
 
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://puppies.db'
-
 @app.route('/')
-
 def index():
-    return render_template('basic.html')
+    return render_template('home.html')
 
-@app.route('/puppies')
+app.config['SECRET_KEY'] = 'mysecretkey'
 
-def puppies():
-    return render_template('puppies.html')
 
-@app.route('/owners')
+class PuppyForm(FlaskForm):
 
-def owners():
-     return render_template('owners.html')
+    name = StringField("What is your puppys name?")
+    breed = StringField("What is your puppys breed?")
+    submit = SubmitField('Submit')
+
+class OwnerForm(FlaskForm):
+    owner_name = StringField("What is the owners name?")
+    submit = SubmitField('Submit')
+
+@app.route('/puppies',methods=['GET','POST'])
+def puppy():
+    form = PuppyForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        session['breed'] = form.breed.data
+
+        return redirect(url_for('thankyou'))
+
+    
+    return render_template('puppy.html', form=form)
+
+@app.route('/owners',methods=['GET','POST'])
+def owner():
+    form = OwnerForm()
+    if form.validate_on_submit():
+        session['owner_name'] = form.owner_name.data
+
+        return redirect(url_for('thankyou'))
+
+    return render_template('owner.html', form=form)
+
+@app.route('/thankyou')
+def thankyou():
+    return render_template('thankyou.html')
+
+if __name__ == '__main__':
+    app.run()
