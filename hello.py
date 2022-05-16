bae7df7b868ba1b23dca09e624ea1127e6520b85
jrwalk16 <jeremywalker1216@Gmail.com>
2022-05-16 09:43:51 -0400

    added db form and began Jupyter notebook

--- hello.py
+++ hello.py
@@ -1,14 +1,38 @@
 
 
-# from tkinter.messagebox import RETRY
+
+from ast import Sub
 from flask import Flask, redirect, url_for, session
 from flask import render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField
-
-
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///puppies.db'
+
+db = SQLAlchemy(app)
+
+class Dogs(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    breed = db.Column(db.String(200), nullable=False)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+
+class DbPuppyForm(FlaskForm):
+    name = StringField("Puppy Name", validators=[DataRequired()])
+    breed = StringField("Puppy Breed", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+
 
 @app.route('/')
 def index():
@@ -29,15 +53,22 @@ class OwnerForm(FlaskForm):
 
 @app.route('/puppies',methods=['GET','POST'])
 def puppy():
-    form = PuppyForm()
+    name = None
+    breed = None
+    form = DbPuppyForm()
     if form.validate_on_submit():
-        session['name'] = form.name.data
-        session['breed'] = form.breed.data
+        dog = Dogs(name=form.name.data, breed=form.breed.data)
+        db.session.add(dog)
+        db.session.commit()
+    name = form.name.data
+    breed = form.breed.data
+    form.name.data = ''
+    form.breed.data = ''
 
-        return redirect(url_for('thankyou'))
+        #  return redirect(url_for('thankyou'))
 
-    
-    return render_template('puppy.html', form=form)
+    our_dogs = Dogs.query.order_by(Dogs.date_added)
+    return render_template('puppy.html', form=form, name=name, breed=breed, our_dogs=our_dogs)
 
 @app.route('/owners',methods=['GET','POST'])
 def owner():
